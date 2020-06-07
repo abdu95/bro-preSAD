64595c5a64c016c4b5beee1196200fd79e233d4c
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-07 18:37:24 +0500

    add some files from 2nd session

--- PreSADSession1/ConsoleAppNetFramework/Program.cs
+++ PreSADSession1/ConsoleAppNetFramework/Program.cs
@@ -10,26 +10,39 @@ namespace ConsoleAppNetFramework
     {
         static void Main(string[] args)
         {
-            var shouldWork = true;
-            while (shouldWork)
+            //var shouldWork = true;
+            //while (shouldWork)
+            //{
+            //    var input = Console.ReadKey();
+            //    if (input.KeyChar == 'e')
+            //    {
+            //        Console.Write(" - Exiting....");
+            //        shouldWork = false;
+            //    }
+            //    else if (input.KeyChar == 'h')
+            //    {
+            //        Console.Write(" - Enter your name:");
+            //        var name = Console.ReadLine();
+            //        Console.WriteLine($"Hi, {name}");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" - Wrong command... Try again \n");
+            //    }
+            //}
+            int value = 5;
+
+            void ChangeValue(int val)
             {
-                var input = Console.ReadKey();
-                if (input.KeyChar == 'e')
-                {
-                    Console.Write(" - Exiting....");
-                    shouldWork = false;
-                }
-                else if (input.KeyChar == 'h')
-                {
-                    Console.Write(" - Enter your name:");
-                    var name = Console.ReadLine();
-                    Console.WriteLine($"Hi, {name}");
-                }
-                else
-                {
-                    Console.Write(" - Wrong command... Try again \n");
-                }
+                int newValue = 10;
+                val = newValue;
             }
-            }
+
+            Console.WriteLine($"Value:  {value}");
+            ChangeValue(value);
+            Console.WriteLine(value);
+            Console.ReadLine();
+
+        }
     }
 }
